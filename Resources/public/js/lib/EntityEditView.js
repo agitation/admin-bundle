6138865a9a93b8533660e3461380dd949cee89a8
Alexander Günsche <mail@lxg.de>
2016-08-21 20:11:29 +0200

    better integration of list and edit views

---
+++ Resources/public/js/lib/EntityEditView.js
@@ -0,0 +1,20 @@
+ag.ns("ag.admin");
+
+(function(){
+var
+    entityEditView = function(blocks)
+    {
+        ag.ui.ctxt.View.call(this, blocks);
+        this.addClass("edit");
+
+        this.one("ag.admin.entity.search", (ev, request) => {
+            var header = this.getBlock(ag.admin.EntityEditHeader);
+            header && header.setRelativeBacklink(true);
+        });
+    };
+
+entityEditView.prototype = Object.create(ag.ui.ctxt.View.prototype);
+
+ag.admin.EntityEditView = entityEditView;
+
+})();
