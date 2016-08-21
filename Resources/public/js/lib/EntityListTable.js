8d7fe86c06a99e5d229f34f904402412427a5a8a
Alexander Günsche <mail@lxg.de>
2016-08-21 21:37:13 +0200

    code style and performance refactoring

--- Resources/public/js/lib/EntityListTable.js
+++ Resources/public/js/lib/EntityListTable.js
@@ -94,186 +94,192 @@ var
                 }
             }
         );
-    };
+    },
 
-ag.admin.EntityListTable = function(exporter, columns, actions)
-{
-    var
-        $elem = ag.ui.tool.tpl("agitadmin-listview", ".listview-table"),
-        $table = $elem.find("table"),
-        $tbody = $table.find("tbody"),
-        $header = $table.find("thead tr"),
-        $tfoot = $table.find("tfoot"),
-        $footer = $tfoot.find("tr"),
 
-        entities = new ag.common.Collection(),
-        rowCount = 0,
+    fillRow = function($row, item)
+    {
+        $row.empty();
 
-        fillRow = function($row, item)
+        if (item.deleted)
+            $row.addClass("deleted");
+        else
+            $row.removeClass("deleted");
+
+        Object.keys(this.columns).forEach(key => {
+            var column = this.columns[key];
+
+            $("<td>")
+                .addClass(column.style + " priority-" + column.priority)
+                .html(column.filter ? column.filter(item, key, column) : item[key])
+                .appendTo($row);
+        });
+
+        if (this.actions.length)
         {
-            $row.empty();
+            var $actionTd = $("<td class='actions'>");
 
-            if (item.deleted)
-                $row.addClass("deleted");
-            else
-                $row.removeClass("deleted");
+            this.actions.forEach(action => {
+                var
+                    $link = $("<a>").append(
+                        $("<i>").addClass(action.icon),
+                        $("<span>").text(action.title)
+                    );
 
-            Object.keys(columns).forEach(key => {
-                var column = columns[key];
+                $link.getTable = () => this;
 
-                $("<td>")
-                    .addClass(column.style + " priority-" + column.priority)
-                    .html(column.filter ? column.filter(item, key, column) : item[key])
-                    .appendTo($row);
+                action.generate($link, item, action);
+                $actionTd.append($link);
             });
 
-            if (actions.length)
-            {
-                var $actionTd = $("<td class='actions'>");
+            $row.append($actionTd);
+        }
+    },
 
-                actions.forEach(action => {
-                    var
-                        $link = $("<a>").append(
-                            $("<i>").addClass(action.icon),
-                            $("<span>").text(action.title)
-                        );
-
-                    $link.getTable = () => $elem;
-
-                    action.generate($link, item, action);
-                    $actionTd.append($link);
-                });
-
-                $row.append($actionTd);
-            }
-        },
-
-        renumberRows = function(rows)
-        {
-            var rowCount = 0;
-
-            rows.each(function(){
-                ++rowCount;
-                $(this).attr("data-num", rowCount).find("td[data-name=num]").text(rowCount);
-            });
-        },
-
-        updateFooter = function()
-        {
-            $tfoot.hide();
-            $footer.empty();
-        };
-
-    $elem.truncate = function()
+    renumberRows = function(rows)
     {
-        entities.truncate();
-        $tbody.empty();
-        rowCount = 0;
-        updateFooter();
-        $elem.showNoResultsRow();
-    };
+        this.rowCount = 0;
 
-    $elem.getItems = function()
+        rows.each(function(){
+            ++this.rowCount;
+            $(this).attr("data-num", this.rowCount).find("td[data-name=num]").text(this.rowCount);
+        });
+    },
+
+    updateFooter = function()
     {
-        return entities;
-    };
+        this.tfoot.hide();
+        this.footTr.empty();
+    },
 
-    $elem.contains = function(id)
-    {
-        return (id && entities.get(id));
-    };
-
-    $elem.addItem = function(item)
-    {
-        var $row = $("<tr>").attr({ "data-num" : ++rowCount, "data-id": item.id });
-
-        entities.length || $tbody.empty();
-        entities.add(item);
-        fillRow($row, item);
-        $row.appendTo($tbody);
-        updateFooter();
-    };
-
-    $elem.updateItem = function(item)
-    {
-        var $row = $tbody.find("tr[data-id=" + item.id + "]");
-
-        entities.update(item);
-        fillRow($row, item);
-        updateFooter();
-    };
-
-    $elem.removeItem = function(id)
-    {
-        entities.remove(id);
-        $tbody.find("tr[data-id=" + id + "]").remove();
-
-        renumberRows($tbody.find("tr[data-num]"));
-        updateFooter();
-        entities.length || $elem.showNoResultsRow();
-    };
-
-    $elem.showNoResultsRow = function()
+    showNoResultsRow = function()
     {
         var $row = ag.ui.tool.tpl("agitadmin-listview", "tr.noresults");
 
-        $row.find("td").attr("colspan", Object.keys(columns).length + (actions.length ? 1 : 0));
+        $row.find("td").attr("colspan", Object.keys(this.columns).length + (this.actions.length ? 1 : 0));
 
-        $tbody.html($row);
-        updateFooter(true);
+        this.tbody.html($row);
+        updateFooter.call(this);
     };
 
-    Object.keys(columns).forEach(key => $header
-        .append($("<th>")
-        .addClass(columns[key].style + " priority-" + columns[key].priority)
-        .html(columns[key].title))
-    );
-
-    actions.length && $header.append($("<th class='actions'>").text(ag.intl.t("Actions")));
-    $elem.showNoResultsRow();
-
-    return $elem;
-};
-
-ag.admin.EntityListTable.getFilter = function(name)
+entityListTable = function(exporter, columns, actions)
 {
-    return ag.admin.EntityListTable._filters[name];
+    // TODO: nodify();
+    this.extend(this, ag.ui.tool.tpl("agitadmin-listview", ".listview-table"));
+
+    var table = this.find("table");
+
+    this.columns = columns;
+    this.actions = actions;
+
+    this.tfoot = table.find("tfoot");
+    this.footTr = this.tfoot.find("tr");
+
+    this.headTr = table.find("thead tr");
+    this.tbody = table.find("tbody");
+    this.entities = new ag.common.Collection();
+    this.rowCount = 0;
+
+    Object.keys(this.columns).forEach(key => this.headTr.append(
+        $("<th>")
+            .addClass(this.columns[key].style + " priority-" + this.columns[key].priority)
+            .html(this.columns[key].title)
+    ));
+
+    this.actions.length && this.headTr.append($("<th class='actions'>").text(ag.intl.t("Actions")));
+    showNoResultsRow.call(this);
 };
 
-ag.admin.EntityListTable.getColumn = function(name, extra)
+entityListTable.prototype = Object.create(ag.ui.ctxt.Block.prototype);
+
+entityListTable.prototype.truncate = function()
+{
+    this.entities.truncate();
+    this.tbody.empty();
+    this.rowCount = 0;
+    updateFooter.call(this);
+    showNoResultsRow.call(this);
+};
+
+entityListTable.prototype.getItems = function()
+{
+    return this.entities;
+};
+
+entityListTable.prototype.contains = function(id)
+{
+    return (id && this.entities.get(id));
+};
+
+entityListTable.prototype.addItem = function(item)
+{
+    var $row = $("<tr>").attr({ "data-num" : ++this.rowCount, "data-id": item.id });
+
+    this.entities.length || this.tbody.empty();
+    this.entities.add(item);
+    fillRow.call(this, $row, item);
+    updateFooter.call(this);
+    $row.appendTo(this.tbody);
+};
+
+entityListTable.prototype.updateItem = function(item)
+{
+    var $row = this.tbody.find("tr[data-id=" + item.id + "]");
+
+    this.entities.update(item);
+    fillRow.call(this, $row, item);
+    updateFooter.call(this);
+};
+
+entityListTable.prototype.removeItem = function(id)
+{
+    this.entities.remove(id);
+    this.tbody.find("tr[data-id=" + id + "]").remove();
+
+    renumberRows(this.tbody.find("tr[data-num]"));
+    updateFooter.call(this);
+    this.entities.length || showNoResultsRow.call(this);
+};
+
+entityListTable.getFilter = function(name)
+{
+    return entityListTable._filters[name];
+};
+
+entityListTable.getColumn = function(name, extra)
 {
     return $.extend(
         {},
-        ag.admin.EntityListTable._columnTpl,
-        ag.admin.EntityListTable._columns[name],
+        entityListTable._columnTpl,
+        entityListTable._columns[name],
         extra || {}
     );
 };
 
-ag.admin.EntityListTable.createColumn = function(options)
+entityListTable.createColumn = function(options)
 {
     return $.extend(
         {},
-        ag.admin.EntityListTable._columnTpl,
+        entityListTable._columnTpl,
         options);
 };
 
-ag.admin.EntityListTable.getAction = function(name, extra)
+entityListTable.getAction = function(name, extra)
 {
     return $.extend(
         {},
-        ag.admin.EntityListTable._actionTpl,
-        ag.admin.EntityListTable._actions[name],
+        entityListTable._actionTpl,
+        entityListTable._actions[name],
         extra || {}
     );
 };
 
-ag.admin.EntityListTable.createAction = function(params)
+entityListTable.createAction = function(params)
 {
-    return $.extend({}, ag.admin.EntityListTable._actionTpl, params);
+    return $.extend({}, entityListTable._actionTpl, params);
 };
 
-ag.admin.EntityListTable._filters =
+entityListTable._filters =
 {
     id : function(item)
     {
@@ -341,24 +347,24 @@ ag.admin.EntityListTable._filters =
     }
 };
 
-ag.admin.EntityListTable._columnTpl = { title : "", filter : () => {}, style : "", priority : 2 };
+entityListTable._columnTpl = { title : "", filter : () => {}, style : "", priority : 2 };
 
-ag.admin.EntityListTable._columns =
+entityListTable._columns =
 {
-    id :            { title : ag.intl.t("ID"), filter: ag.admin.EntityListTable._filters.id, style: "right", priority : 1 },
+    id :            { title : ag.intl.t("ID"), filter: entityListTable._filters.id, style: "right", priority : 1 },
     num :           { title : "#", style: "right", priority : 1 },
-    name :          { title : ag.intl.t("Name"), filter: ag.admin.EntityListTable._filters.text, priority : 1 },
-    date :          { title : ag.intl.t("Date"), filter: ag.admin.EntityListTable._filters.date },
-    datetime :      { title : ag.intl.t("Date"), filter: ag.admin.EntityListTable._filters.datetime },
-    description :   { title : ag.intl.t("Description"), filter: ag.admin.EntityListTable._filters.text, style: "longtext", priority : 3 },
-    reference :     { title : "", filter: ag.admin.EntityListTable._filters.reference, priority : 2 },
-    check :         { title : "", style: "center", filter: ag.admin.EntityListTable._filters.check, priority : 2 },
-    user :          { title : ag.intl.t("User"), filter: ag.admin.EntityListTable._filters.user, priority : 2 }
+    name :          { title : ag.intl.t("Name"), filter: entityListTable._filters.text, priority : 1 },
+    date :          { title : ag.intl.t("Date"), filter: entityListTable._filters.date },
+    datetime :      { title : ag.intl.t("Date"), filter: entityListTable._filters.datetime },
+    description :   { title : ag.intl.t("Description"), filter: entityListTable._filters.text, style: "longtext", priority : 3 },
+    reference :     { title : "", filter: entityListTable._filters.reference, priority : 2 },
+    check :         { title : "", style: "center", filter: entityListTable._filters.check, priority : 2 },
+    user :          { title : ag.intl.t("User"), filter: entityListTable._filters.user, priority : 2 }
 };
 
-ag.admin.EntityListTable._actionTpl = { title : "", href : "", generate : () => {}, icon : "", params : {} };
+entityListTable._actionTpl = { title : "", href : "", generate : () => {}, icon : "", params : {} };
 
-ag.admin.EntityListTable._actions =
+entityListTable._actions =
 {
     edit : {
         title: ag.intl.t("edit"),
@@ -408,7 +414,6 @@ ag.admin.EntityListTable._actions =
         title : ag.intl.t("delete"),
         icon : "fa fa-trash",
         generate : function($link, item) {
-
             var actionType = "delete";
 
             if (item.deleted)
@@ -442,5 +447,7 @@ ag.admin.EntityListTable._actions =
         }
     }
 };
+
+ag.admin.EntityListTable = entityListTable;
 
 })();
