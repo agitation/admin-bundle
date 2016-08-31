e3bb8e16a8ce7754be3c05b39817e0560227bb49
Alexander Günsche <mail@lxg.de>
2016-08-31 13:55:30 +0200

    migrate map to Leaflet

--- Resources/public/js/lib/field/Location.js
+++ Resources/public/js/lib/field/Location.js
@@ -3,47 +3,40 @@ ag.ns("ag.admin.field");
 
 (function(){
     var
-        defaultLonLat = [10, 51],
+        defaultLonLat = [51, 10],
+        defaultZoom = 5,
+        locationZoom = 14,
 
         locationField = function()
         {
-            var
-                $map = new ag.ui.elem.Map(),
-                $marker = ag.ui.tool.tpl("agitadmin-location-field", ".location-marker");
+            var map = new ag.ui.elem.Map();
 
-            this.extend(this, ag.ui.tool.tpl("agitadmin-location-field", ".location-field")).prepend($map);
+            this.extend(this, ag.ui.tool.tpl("agitadmin-location-field", ".location-field")).prepend(map);
 
-            this.map = $map.ol;
+            map.addClass("select");
 
-            this.marker = new ol.Overlay({
-                element: $marker[0],
-                positioning: "bottom-center",
-                stopEvent: false
-            });
-
+            this.map = map;
+            this.marker = map.createMarker();
             this.currentLocation = null;
 
-            this.map.addOverlay(this.marker);
-
-            this.map.on("click", ev => {
-                var location = ol.proj.transform(ev.coordinate, "EPSG:3857", "EPSG:4326");
-                this.currentLocation = { lon : location[0], lat : location[1] };
-                this.marker.setPosition(ev.coordinate);
+            this.map.lMap.on("click", ev => {
+                var location = ev.latlng;
+                this.currentLocation = { lon : location.lng, lat : location.lat };
+                this.marker.setLatLng(location);
             });
         };
 
     locationField.prototype = Object.create(ag.ui.field.ComplexField.prototype);
 
-    locationField.prototype.setValue = function(value)
+    locationField.prototype.setValue = function(location)
     {
-        if (value instanceof Object && value.lon && value.lat)
+        if (location instanceof Object && location.lon && location.lat)
         {
-            var location = ol.proj.transform([value.lon, value.lat], "EPSG:4326", "EPSG:3857");
+            var latlng = [ location.lat, location.lon ];
 
-            this.marker.setPosition(location);
-            this.map.getView().setCenter(location);
-            this.map.getView().setZoom(15);
-            this.currentLocation = value;
+            this.marker.setOpacity(1).setLatLng(latlng);
+            this.map.lMap.setView(latlng, locationZoom);
+            this.currentLocation = location;
         }
         else
         {
@@ -56,19 +49,22 @@ ag.ns("ag.admin.field");
     locationField.prototype.clear = function()
     {
         this.currentLocation = null;
-        this.marker.setPosition(); // hide marker
-        this.map.getView().setCenter(ol.proj.transform(defaultLonLat, "EPSG:4326", "EPSG:3857"));
-        this.map.getView().setZoom(4);
+        this.marker.setOpacity(0);
+        this.map.lMap.setView(defaultLonLat, defaultZoom);
+        this.map.lMap.setZoom(4);
 
         return this;
     };
 
-    locationField.prototype.getValue = function()
+    locationField.prototype.validate = function()
     {
         if (!this.currentLocation)
             throw new ag.ui.error.FormError(ag.intl.t("Please select a location."));
+    };
 
-        return new ag.api.Object("common.v1/Location", this.currentLocation);
+    locationField.prototype.getValue = function()
+    {
+        return new ag.api.Object("common.v1/Location", this.currentLocation || {});
     };
 
     ag.admin.field.Location = locationField;
