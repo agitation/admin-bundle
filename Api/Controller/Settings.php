fb3dc510ad7039331fb74f5c2f757209318fde1e
Alexander Günsche <mail@lxg.de>
2016-09-15 16:10:26 +0200

    use constructor instead of global injection

--- Api/Controller/Settings.php
+++ Api/Controller/Settings.php
@@ -13,23 +13,31 @@ use Agit\ApiBundle\Annotation\Controller\Controller;
 use Agit\ApiBundle\Annotation\Endpoint;
 use Agit\ApiBundle\Annotation\Depends;
 use Agit\ApiBundle\Api\Controller\AbstractController;
+use Agit\SettingBundle\Service\SettingService;
 
 /**
  * @Controller(namespace="admin.v1")
+ * @Depends({"@agit.setting"})
  */
 class Settings extends AbstractController
 {
+    private $settingService;
+
+    public function __construct(SettingService $settingService)
+    {
+        $this->settingService = $settingService;
+    }
+
     /**
      * @Endpoint\Endpoint(request="common.v1/String[]",response="Setting[]")
      * @Endpoint\Security(capability="agit.setting.read")
-     * @Depends({"@agit.setting"})
      *
      * Load application settings by setting names.
      */
     public function load(array $names)
     {
         $result = [];
-        $settingList = $this->getService("agit.setting")->getSettings($names);
+        $settingList = $this->settingService->getSettings($names);
 
         foreach ($settingList as $setting) {
             $result[] = $this->createObject("Setting", (object) [
@@ -43,7 +51,6 @@ class Settings extends AbstractController
     /**
      * @Endpoint\Endpoint(request="Setting[]",response="Setting[]")
      * @Endpoint\Security(capability="agit.setting.write")
-     * @Depends({"@agit.setting"})
      *
      * Save application settings.
      */
@@ -55,13 +62,13 @@ class Settings extends AbstractController
             $settings[$apiSetting->get("id")] = $apiSetting->get("value");
         }
 
-        $settingList = $this->getService("agit.setting")->getSettings(array_keys($settings));
+        $settingList = $this->settingService->getSettings(array_keys($settings));
 
         foreach ($settingList as $setting) {
             $setting->setValue($settings[$setting->getId()]);
         }
 
-        $this->getService("agit.setting")->saveSettings($settingList);
+        $this->settingService->saveSettings($settingList);
 
         return $this->load(array_keys($settings));
     }
