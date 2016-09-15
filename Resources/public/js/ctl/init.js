283e932a66cd8e0647f04a3f7aac8ae7d43c08a4
Alexander Günsche <mail@lxg.de>
2016-09-15 12:20:30 +0200

    include preloader from base-bundle

--- Resources/public/js/ctl/init.js
+++ Resources/public/js/ctl/init.js
@@ -7,12 +7,5 @@ $(document).ready(function() {
     ag.srv("messageHandler", msgH);
     ag.srv("indicator", ind);
     ag.srv("api", new ag.api.Api(ind, msgH));
-    ag.srv("preloader", new ag.api.Preloader());
-
-    $('body').tooltip({
-        selector: "[data-toggle='tooltip']",
-        template: ag.ui.tool.tpl("agitadmin-tooltip", ".tooltip"),
-        container: 'body',
-        animation : false
-    });
+    ag.srv("preloader", new ag.admin.Preloader());
 });
