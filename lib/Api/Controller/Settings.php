bfbe3827e47f5513715c7176b2d8cfaeb07bb9a8
Alexander Günsche <mail@lxg.de>
2016-09-27 17:25:35 +0200

    move setting modification logging into setting-bundle directly

--- lib/Api/Controller/Settings.php
+++ lib/Api/Controller/Settings.php
@@ -14,24 +14,19 @@ use Agit\ApiBundle\Annotation\Depends;
 use Agit\ApiBundle\Annotation\Endpoint;
 use Agit\ApiBundle\Api\Controller\AbstractController;
 use Agit\IntlBundle\Tool\Translate;
-use Agit\LoggingBundle\Service\Logger;
 use Agit\SettingBundle\Service\SettingService;
-use Psr\Log\LogLevel;
 
 /**
  * @Controller(namespace="admin.v1")
- * @Depends({"@agit.setting", "@agit.logger"})
+ * @Depends({"@agit.setting"})
  */
 class Settings extends AbstractController
 {
     private $settingService;
 
-    private $logger;
-
-    public function __construct(SettingService $settingService, Logger $logger)
+    public function __construct(SettingService $settingService)
     {
         $this->settingService = $settingService;
-        $this->logger = $logger;
     }
 
     /**
@@ -66,26 +61,7 @@ class Settings extends AbstractController
             $settings[$entry->get("id")] = $entry->get("value");
         }
 
-        $oldValues = $this->settingService->getValuesOf(array_keys($settings));
         $this->settingService->saveSettings($settings);
-        $newValues = $this->settingService->getValuesOf(array_keys($settings));
-
-        $changedSettings = [];
-
-        foreach ($oldValues as $key => $value) {
-            if ($oldValues[$key] !== $newValues[$key]) {
-                $changedSettings[] = $this->settingService->getNameOf($key);
-            }
-        }
-
-        if (count($changedSettings)) {
-            $this->logger->log(
-                LogLevel::NOTICE,
-                "agit.settings",
-                sprintf(Translate::tl("The following settings have been changed: %s"), implode(", ", $changedSettings)),
-                true
-            );
-        }
 
         return $this->load(array_keys($settings));
     }
