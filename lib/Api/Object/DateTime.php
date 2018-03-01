dfab252cb72449de1158ce772b995d510467f3f5
Alexander Günsche <mail@lxg.de>
2018-03-01 12:21:55 +0100

    add API DateTime object with seconds

---
+++ lib/Api/Object/DateTime.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+/*
+ * @package    agitation/admin-bundle
+ * @link       http://github.com/agitation/admin-bundle
+ * @author     Alexander Günsche
+ * @license    http://opensource.org/licenses/MIT
+ */
+
+namespace Agit\AdminBundle\Api\Object;
+
+use Agit\ApiBundle\Annotation\Object;
+use Agit\ApiBundle\Api\Object\DateTime as BaseDateTime;
+use Agit\ApiBundle\Annotation\Property;
+
+/**
+ * @Object\Object(namespace="admin.v1")
+ *
+ * DateTime object which includes seconds. Useful for logs etc.
+ */
+class DateTime extends BaseDateTime
+{
+    /**
+     * @Property\Name("Second")
+     * @Property\IntegerType(minValue=0, maxValue=59)
+     */
+    public $second;
+
+    public function __toString()
+    {
+        return sprintf('%04d-%02d-%02d %02d:%02d:00', $this->year, $this->month, $this->day, $this->hour, $this->minute, $this->second);
+    }
+
+    public function fill($dateTime)
+    {
+        parent::fill($dateTime);
+
+        if ($dateTime instanceof \DateTime)
+        {
+            $this->second = (int) $dateTime->format('s');
+        }
+    }
+}
